cbeb3d1aa8eae6f4bbd840804647aec01d20b1c0
Anders Dalsgaard Madsen <ande48d0@edu.eal.dk>
2019-02-21 11:55:43 +0100

    Oprettelse af DBController & Controller.

---
+++ PrettyHair/DBController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair
+{
+    class DBController
+    {
+        private static string connectionString =
+        "Server = ealSQL1.eal.local; Database = A_DB29_2018; User Id = A_STUDENT29; Password = A_OPENDB29;";
+    }
+}
